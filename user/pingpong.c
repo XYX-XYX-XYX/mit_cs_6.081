298f2ea205228dc322e6d7efff2683ce2379d37a
xyx <xu13636004980@icloud.com>
2024-02-01 12:31:58 +0800

    Done with pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -7,6 +7,7 @@ main(int argc , char** argv){
     int p1[2],p2[2];
     char buff[1];
     int pid;
+    buff[0] = '!';
 
     pipe(p1);
     pipe(p2);
@@ -14,23 +15,23 @@ main(int argc , char** argv){
     if( (pid = fork()) < 0 ){
         printf("error!");
         exit(1);
-    }else if(pid == 0){
+    }else if(pid != 0){
         close(p1[0]);
-        read(p1[1],buff,1);
+        write(p1[1],"p",1);
         close(p1[1]);
-        printf("%d: received ping\n",getpid());
-        close(p2[0]);
-        write(p2[1]," ",1);
-        close(p2[1]);
-        exit(0);
-    }else{
-        close(p1[1]);
-        write(p1[0]," ",1);
-        close(p1[0]);
         close(p2[1]);
         read(p2[0],buff,1);
         close(p2[0]);
         printf("%d: received pong\n",getpid());
+    }else{
+        close(p1[1]);
+        read(p1[0],buff,1);
+        close(p1[0]);
+        printf("%d: received ping\n",getpid());
+        close(p2[0]);
+        write(p2[1],"c",1);
+        close(p2[1]);
+        exit(0);
     }
     exit(0);
 }
